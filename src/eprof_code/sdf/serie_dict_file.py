8fb74892deb9b225f678538cfda66d21625bb2e4
crazyhouse33 <hugo.alonso@etu.u-bordeaux.fr>
2020-06-27 22:35:01 +0200

    commit2 for tests

--- src/eprof_code/sdf/serie_dict_file.py
+++ src/eprof_code/sdf/serie_dict_file.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as pyplot
-class Super_dict:
+class Serie_dict:
     def __init__(self, file_or_dico=None, key_sep=':', value_sep=' '):
         """Create a super dict from a normal dict, or a file. Empty super dict if nothing"""
         self.key_sep=key_sep;
@@ -12,18 +12,23 @@ class Super_dict:
             self.dico = parse_file(file_or_dico)
 
     
-    def dump_to_file(self, file, keys=None):
+    def dump_to_file(self, file, keys=None, key_sep=None, value_sep=None):
         """Serialize subset of keys (default all) to given path"""
         if keys == None:
             keys= self.dico.keys()
+        if key_sep == None:
+            key_sep= self.key_sep
+
+        if value_sep == None:
+            value_sep= self.key_sep
 
         with open(file) as f:
             for key in keys:
-                print("{}{}{}".format(key, self.key_sep, self.value_sep.join(self.dico[key])),file=f)
+                print("{}{}{}".format(key, key_sep, value_sep.join(self.dico[key])),file=f)
 
     def parse_file(file, key_sep, value_sep):
         """Parse a file and return a super dict"""
-        dico=Super_dict()
+        dico=Serie_dict()
         with open(file) as f:
             for line in f.read().split('\n'):
                 key, sep, values= line.partition(key_sep)
